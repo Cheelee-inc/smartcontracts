0423c3310f4e524817244e0669dd066e2ef48e46
rs-Nenad <97531280+rs-Nenad@users.noreply.github.com>
2022-12-16 10:09:03 +0300

    remove hardhat/console

--- contracts/MultiVesting.sol
+++ contracts/MultiVesting.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IVesting.sol";
-import "hardhat/console.sol";
 
 
 /// @title MultiVesting
