08876b5e7e5d333362c71b1d975dc9010c5d0c2b
Zemtsov Igor <i.zemtsov@nutson.us>
2023-03-15 17:25:39 +0100

    upgraded contracts in int

--- config/ContractsConfig.ts
+++ config/ContractsConfig.ts
@@ -8,10 +8,10 @@ export const CommonBlacklistConfig: CommonBlacklistInterface = {
   testnet: Networks.bscTestnet,
   mainnet: Networks.bscMainnet,
   contractName: 'CommonBlacklist',
-  multiSigAddress: '0x126481E4E79cBc8b4199911342861F7535e76EE7',
-  contractAddress: '0xe95C8Bb4Da1561B3aC9c5719716A11055c2655c3',
-  proxyContractAddress: '0x7C1E145346Cb97BeeD131ce541d0497Bada9f1DF',
-  adminContractAddress: '0x1cf4dF9dA9062Fd7c441a9B77c6bcF45596492bB',
+  multiSigAddress: '0xe69C24fA49FC2fF52305E4300D627a9094b648f5',
+  contractAddress: '0x5c01f5AFF26Abb2d444e71B3f4321E408dbea1EB',
+  proxyContractAddress: '0xf7c0F7d756283349438ae2278DF389e5eC3bb2d2',
+  adminContractAddress: '0xe9B36a6cacB369E7eee08d595720662425454bc2',
 };
 
 export const LEEConfig: LEEConfigInterface = {
@@ -21,11 +21,11 @@ export const LEEConfig: LEEConfigInterface = {
   tokenName: 'CHEELEE Attention Token',
   tokenSymbol: 'LEE',
   maxAmount: 7000000000,
-  blackList: '0x7C1E145346Cb97BeeD131ce541d0497Bada9f1DF',
-  multiSigAddress: '0xE6e74cA74e2209A5f2272f531627f44d34AFc299',
-  contractAddress: '0x7Fa8d22c2d8EBeFcE3595B38EE7FDbab0F697D6f',
-  proxyContractAddress: '0x5b72798a4F4983D6e32215125374A8E2a39cc2de',
-  adminContractAddress: '0x1cf4dF9dA9062Fd7c441a9B77c6bcF45596492bB',
+  blackList: '0xf7c0F7d756283349438ae2278DF389e5eC3bb2d2',
+  multiSigAddress: '0xe69C24fA49FC2fF52305E4300D627a9094b648f5',
+  contractAddress: '0x99E6E770ccB85fca08B4990F763C5491c56D3384',
+  proxyContractAddress: '0xB6be99BEDBa40F0606199534977dE2b6D5857bfe',
+  adminContractAddress: '0xe9B36a6cacB369E7eee08d595720662425454bc2',
 };
 
 export const CHEELConfig: CHEELConfigInterface = {
@@ -35,11 +35,11 @@ export const CHEELConfig: CHEELConfigInterface = {
   tokenName: 'CHEELEE',
   tokenSymbol: 'CHEEL',
   maxAmount: 1000000000,
-  blackList: '0x7C1E145346Cb97BeeD131ce541d0497Bada9f1DF',
-  multiSigAddress: '0x126481E4E79cBc8b4199911342861F7535e76EE7',
-  contractAddress: '0xFC510c8e5C7973F8fbaeb2b20b9e686d05851abf',
-  proxyContractAddress: '0x439CBe5d6cb1ade400BF2a83f207Bdc491b4fd0d',
-  adminContractAddress: '0x1cf4dF9dA9062Fd7c441a9B77c6bcF45596492bB',
+  blackList: '0xf7c0F7d756283349438ae2278DF389e5eC3bb2d2',
+  multiSigAddress: '0xe69C24fA49FC2fF52305E4300D627a9094b648f5',
+  contractAddress: '0x79a2296D3F48F9A9a267a888F77e5401624c89d2',
+  proxyContractAddress: '0xACC7f64ea52C947Ca838bE1Fb274FC477cFE8791',
+  adminContractAddress: '0xe9B36a6cacB369E7eee08d595720662425454bc2',
 };
 
 export const NFTGlassesConfig: NFTConfigInterface = {
@@ -48,11 +48,11 @@ export const NFTGlassesConfig: NFTConfigInterface = {
   contractName: 'NFT',
   nftName: 'CHEELEE NFT Glasses',
   nftSymbol: 'CNFTG',
-  blackList: '0x7C1E145346Cb97BeeD131ce541d0497Bada9f1DF',
-  multiSigAddress: '0xC40b7fBb7160B98323159BA800e122C9DeD0668D',
-  contractAddress: '0x4B9a05F7B88596EaF752Bc2fc7617ac998e3a91C',
-  proxyContractAddress: '0x6484E027BDA3B082FE04dAf76e36C973B3352d45',
-  adminContractAddress: '0x1cf4dF9dA9062Fd7c441a9B77c6bcF45596492bB',
+  blackList: '0xf7c0F7d756283349438ae2278DF389e5eC3bb2d2',
+  multiSigAddress: '0xe69C24fA49FC2fF52305E4300D627a9094b648f5',
+  contractAddress: '0x13cFCc486A34595Df30DF5A4507d65A72126b464',
+  proxyContractAddress: '0xC76415525c3C7F3c226B204660143823482e8fCD',
+  adminContractAddress: '0xe9B36a6cacB369E7eee08d595720662425454bc2',
 };
 
 export const NFTCasesConfig: NFTConfigInterface = {
@@ -61,11 +61,11 @@ export const NFTCasesConfig: NFTConfigInterface = {
   contractName: 'NFT',
   nftName: 'CHEELEE NFT Cases',
   nftSymbol: 'CNFTC',
-  blackList: '0x7C1E145346Cb97BeeD131ce541d0497Bada9f1DF',
-  multiSigAddress: '0xC40b7fBb7160B98323159BA800e122C9DeD0668D',
-  contractAddress: '0x4B9a05F7B88596EaF752Bc2fc7617ac998e3a91C',
-  proxyContractAddress: '0x41d2bA27d97f026968De00F034580Fd0dd8d4333',
-  adminContractAddress: '0x1cf4dF9dA9062Fd7c441a9B77c6bcF45596492bB',
+  blackList: '0xf7c0F7d756283349438ae2278DF389e5eC3bb2d2',
+  multiSigAddress: '0xe69C24fA49FC2fF52305E4300D627a9094b648f5',
+  contractAddress: '0x13cFCc486A34595Df30DF5A4507d65A72126b464',
+  proxyContractAddress: '0xcF87261003F389cF6F0b06f9511C43E155cE5D72',
+  adminContractAddress: '0xe9B36a6cacB369E7eee08d595720662425454bc2',
 };
 
 export const NFTSaleConfig: NFTSaleConfigInterface = {
@@ -75,7 +75,7 @@ export const NFTSaleConfig: NFTSaleConfigInterface = {
   multiSigAddress: '0x841005214049dcE3168CF8a323DD742BcfbF1dc4',
   contractAddress: '0x0645E0F7eE8ED0F20F3fC1DBFE81829Dd33CbFf2',
   proxyContractAddress: '0x252FE80eC028aAfEB3925f8233a9ef024aDfdDfA',
-  adminContractAddress: '0x1cf4dF9dA9062Fd7c441a9B77c6bcF45596492bB',
+  adminContractAddress: '0xe9B36a6cacB369E7eee08d595720662425454bc2',
 };
 
 export const TreasuryConfig: TreasuryConfigInterface = {
@@ -83,7 +83,7 @@ export const TreasuryConfig: TreasuryConfigInterface = {
   mainnet: Networks.bscMainnet,
   contractName: 'Treasury',
   multiSigAddress: '0xe69C24fA49FC2fF52305E4300D627a9094b648f5',
-  contractAddress: '0xD43722bCfa6C8274c25878627371d7Ac51235bB9',
-  proxyContractAddress: '0x703FEaa4eCadc96673a06C31c4053Fd014272999',
-  adminContractAddress: '0x1cf4dF9dA9062Fd7c441a9B77c6bcF45596492bB',
+  contractAddress: '0x31b9F82C5C160297Bd56c7eFD0e8a0A62077062e',
+  proxyContractAddress: '0x54b2647c151990aC9E6bBe1cf39d5571D562c87c',
+  adminContractAddress: '0xe9B36a6cacB369E7eee08d595720662425454bc2',
 };
