7d7c3e279938b43bce7a3c3f907b1f2e530240fb
rs-Nenad <jovanovski.n@redsteep.com>
2023-05-23 17:46:47 +0300

    deploy multivesting scripts

--- scripts/deploy_multivesting.ts
+++ scripts/deploy_multivesting.ts
@@ -2,16 +2,15 @@ import { ethers, upgrades } from "hardhat";
 import { verify } from "./19_verify";
 
 async function main() {
+    let token = ""
     const MV = await ethers.getContractFactory("MultiVesting");
-    const mv = await MV.deploy("0xeE395644EeA4b8e6A3A4e17A3616A5F1B0Bc4634", true, false, 1800, 3600);
+    const mv = await MV.deploy(token, true, false, 900, 1800);
     await mv.deployed();
-    console.log("MV", mv.address);
+    console.log("Multivesting", mv.address);
 
-    await verify(mv.address, ["0xeE395644EeA4b8e6A3A4e17A3616A5F1B0Bc4634", true, false, 1800, 3600])
+    await verify(mv.address, [token, true, false, 900, 1800])
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
